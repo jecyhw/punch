6c9262044d90fe705b669395545954af5f30332c
jecyhw <1147352923@qq.com>
2018-01-01 11:14:04 +0800

    ADD: server host

--- src/main/java/com/jecyhw/punch/ServerProperties.java
+++ src/main/java/com/jecyhw/punch/ServerProperties.java
@@ -3,12 +3,15 @@ package com.jecyhw.punch;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
-@Setter
-@Getter
+
 @Component
 @ConfigurationProperties(prefix = "punch.server")
+@Setter
+@Getter
+@Order(value = 0)
 public class ServerProperties {
     private String host;
 }
